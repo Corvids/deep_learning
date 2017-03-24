ba8a5eb73799358365c12c4f9f2a6b449e2f54e2
Corvids <joonnifer@gmail.com>
2017-03-24 16:08:00 -0500

    update regression model for gapminder

--- regression/gapminder1.py
+++ regression/gapminder1.py
@@ -1,14 +1,17 @@
 # TODO: Add import statements
-import 
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 # Assign the dataframe to this variable.
 # TODO: Load the data
-bmi_life_data = None
+bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv', index_col='Country')
+y_values, x_values = bmi_life_data[['Life expectancy']], bmi_life_data[['BMI']]
 
 # Make and fit the linear regression model
 #TODO: Fit the model and Assign it to bmi_life_model
-bmi_life_model = None
+bmi_life_model = LinearRegression()
+bmi_life_model.fit(x_values, y_values)
 
 # Mak a prediction using the model
 # TODO: Predict life expectancy for a BMI value of 21.07931
-laos_life_exp = None
+laos_life_exp = bmi_life_model.predict(21.07931)
